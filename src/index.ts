4dec5ca8cefd012dc40ee5aef13f8f0bf6da88de
adityavn03 <aditya.vn03@gmail.com>
2026-04-17 16:47:26 +0000

    main

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("Hello World 🚀");
+});
+
+// Another route
+app.get("/about", (req, res) => {
+  res.json({
+    message: "This is a basic Express server"
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
